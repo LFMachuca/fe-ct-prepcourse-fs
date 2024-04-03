eb426f68c5c74b5617b2b4385970479e2ccf7ac3
LFMachuca <fmigra87@gmail.com>
2024-04-02 21:12:41 -0300

    Ejercicios de objetos resueltos hasta el 11

--- C8 Objects + Callbacks/objects/ejercicios/07.js
+++ C8 Objects + Callbacks/objects/ejercicios/07.js
@@ -7,6 +7,18 @@ function sort(sortBy, list) {
    // recibes --> ("a", [{ a: 1, b: 3 }, { a: 3, b: 2 }, { a: 2, b: 40 }])
    // retorna --> [{ a: 3, b: 2 }, { a: 2, b: 40 }, { a: 1, b: 3 }]
    // Tu código:
+
+   for (let i = 0;i < list.length - 1;i++) {
+      for (let j = 0; j < list.length - 1 - i; j++) {
+        if (list [j][sortBy] < list[j +1][sortBy]) {
+         var temp= list[j];
+         list[j] = list[j +1];
+         list[j + 1] = temp;
+        }
+      }
+      
+   }
+    return list;
 }
 
 module.exports = sort;
